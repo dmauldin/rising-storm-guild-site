1b0cc2d72f8d6a03fd2e6f12c3ba642bd581fc96
David Mauldin <davem@strudel.local>
2009-01-07 18:35:23 -0800

    Fixed bug with empty toon filter and non-empty item filter

--- public/javascripts/tablefilter.js
+++ public/javascripts/tablefilter.js
@@ -12,15 +12,23 @@ $(document).ready(function(){
 filter_loot = function(){
   toons = $("#toon_filter").val().toLowerCase().split(" ");
   items = $("#item_filter").val().toLowerCase().split(" ");
-  $(".filterable tr:visible").hide();
-  $.each(toons, function(){
-    if (this.length > 2) {
-      $(".filterable tr:hidden .toon_name:contains('" + this + "')").parent().show();
+  if ($("#toon_filter").val() == "" && $("#item_filter").val() == "") {
+    $(".filterable tr").show();
+  } else {
+    if ($("#toon_filter").val() != "") {
+      $(".filterable tr:visible").hide();
+      $.each(toons, function(){
+        if (this.length > 2) {
+          $(".filterable tr:hidden .toon_name:contains('" + this + "')").parent().show();
+        }
+      });
+    } else {
+      $(".filterable tr:hidden").show();
     }
-  });
-  $.each(items, function(){
-    if (this.length > 2) {
-      $(".filterable tr:visible .item_name:not(:contains('" + this + "'))").parent().hide();
-    }
-  });
+    $.each(items, function(){
+      if (this.length > 2) {
+        $(".filterable tr:visible .item_name:not(:contains('" + this + "'))").parent().hide();
+      }
+    });
+  }
 }
