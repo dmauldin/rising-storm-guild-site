5c1f82d221644e0cb8a61226bd89694124da93e3
David Mauldin <dmauldin@gmail.com>
2009-03-25 13:12:25 -0700

    attempting to cache achievement list on production

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,4 +1,5 @@
 class AchievementsController < ApplicationController
+  caches_page :index
   def index
     @achievements = Achievement.find([2137, 2138], :include => {:criterias => :toons}).collect {|a| a.criterias}.flatten
   end
