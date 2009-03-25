eedd8c87178504d25571305454db104d85fae247
David Mauldin <dmauldin@gmail.com>
2009-03-25 13:08:57 -0700

    Massive speed improvement on achievement missing list.

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,5 +1,5 @@
 class AchievementsController < ApplicationController
   def index
-    @achievements = Achievement.find([2137, 2138]).collect {|a| a.criterias}.flatten
+    @achievements = Achievement.find([2137, 2138], :include => {:criterias => :toons}).collect {|a| a.criterias}.flatten
   end
 end
