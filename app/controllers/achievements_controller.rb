5a725ddfbe76e1c4635d83c2c6567f9024a151b0
David Mauldin <dmauldin@gmail.com>
2009-05-05 18:07:52 -0700

    started work on achievement generalization updated annotations added acts_as_tree for forum parenting

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,8 +1,9 @@
 class AchievementsController < ApplicationController
   def index
-    @normal_achievements = Achievement.find(2137, :include => {:criterias => {:toons => :job}}).criterias
-    @heroic_achievements = Achievement.find(2138, :include => {:criterias => {:toons => :job}}).criterias
-    @total_toons = Toon.raiders(:include => :job).size.to_f
+    # @normal_achievements = Achievement.find(2137, :include => {:criterias => {:toons => :job}}).criterias
+    # @heroic_achievements = Achievement.find(2138, :include => {:criterias => {:toons => :job}}).criterias
+    # @total_toons = Toon.raiders(:include => :job).size.to_f
+    @achievements = Achievement.all
   end
   
   def show
--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,4 +1,19 @@
+# == Schema Information
+# Schema version: 20090409013015
+#
+# Table name: forums
+#
+#  id           :integer(4)      not null, primary key
+#  title        :string(255)
+#  description  :text
+#  parent_id    :integer(4)
+#  allow_topics :boolean(1)
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
 class Forum < ActiveRecord::Base
+  acts_as_tree
   has_many :topics
   has_many :posts, :through => :topics
   
